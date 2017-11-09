fbf760b5ebaa3e65830455895ec03989cf9f9997
Simanzotti <marcelmanzotti@hotmail.com>
2017-11-09 14:49:43 -0200

    tabela funcionando

--- www/pages/teste.php
+++ www/pages/teste.php
@@ -1,29 +1,134 @@
 ﻿<?php
-include ("pages/conexao.php");
+//include ("pages/conexao.php");
 
-$consulta = "SELECT NOME FROM TB_ADM";
+$servidor = "localhost";
+$nome_usuario = "dispense_banco";
+$senha_usuario = "788898Iamand";
+$nome_do_banco = "dispense_banco";
+$conecta = mysql_connect("$servidor", "$nome_usuario", "$senha_usuario") or die (mysql_error());
+mysql_select_db("$nome_do_banco",$conecta) or die (mysql_error());
 
-$con = $mysqli->query($consulta) or die($mysqli->error);
+$sql = mysql_query("SELECT * FROM TB_ADM");
+
+
+$tipo_usu = "ff";
+
 ?>
 
 <html>
-    <head>
-        <meta charset="utf8">
-    </head>
-    <body>
-        <table>
-            <tr>
-                <td>Nome</td>
-                <td>Senha</td>
-                <td>Perfil</td>
-            </tr>
-            <?php while ($dado = $con->fetch_array()){ ?>
-                <tr>
-                    <td><?php echo $dado["NOME"]; ?></td>
-                    <td><?php echo $dado["SENHA"]; ?></td>
-                    <td><?php echo $dado["PERFIL"]; ?></td>
-                </tr>
-            <?php }?>
-        </table>
-    </body>
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <meta name="format-detection" content="telephone=no">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">
+    <link rel="stylesheet" type="text/css" href="../bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../css/home.css">
+    <link rel="stylesheet" type="text/css" href="../css/gerencial.css">
+</head>
+
+<body class="background">
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12" id="header">
+            <div class="logo">Dispenser<span>APP</span></div>
+        </div>
+
+        <div class="col-12" id="cadastro">
+            <div class="andre">
+                <div class="container">
+                    <h2 class="title">Cadastrar um usuário:</h2>
+                </div>
+
+                <table>
+                    <tr>
+                        <td>Nome</td>
+                        <td>Senha</td>
+                        <td>Perfil</td>
+                    </tr>
+                    <?php while($n = mysql_fetch_array($sql)){ ?>
+                        <tr>
+                            <td><?php echo $n["NOME"]; ?></td>
+                            <td><?php echo $n["SENHA"]; ?></td>
+                            <td><?php echo $n["PERFIL"]; ?></td>
+                        </tr>
+                    <?php }?>
+                </table>
+
+                <?php
+                $sucesso = $_GET["sucesso"];
+
+                if(!is_null($sucesso) && !empty($sucesso) && $sucesso == 1) {
+                    ?>
+                    <p>sucesso</p>
+                <?php } ?>
+
+                <form name="cad-usuario" action="form_gerencial.php" method="POST">
+                    <div class="container">
+
+                        <div class="item">
+                            <label class="form__label">Usuário:</label>
+                            <input class="form__input" type="text" placeholder="Usuário" name="uname" required>
+                        </div>
+                        <div class="item">
+                            <label class="form__label">Senha:</label>
+                            <input class="form__input" type="password" placeholder="Senha" name="psw" required>
+                        </div>
+                        <div class="item">
+                            <label class="form__label">Perfil:</label>
+                            <select name="perfil">
+                                <option value="adm">ADM</option>
+                                <option value="usuario">Usuário</option>
+                            </select>
+                        </div>
+                        <?php if($tipo_usu == "adm") {?>
+                        <div class="item">
+                            <button class="botao-cadastrar cadastrar-blue"> <span class="user" style=" width: 28PX;
+                                height: 28PX;
+                                margin-bottom: -6px;
+                                margin-right: 10px;"></span> Cadastrar!</button>
+                        </div>
+                        <?php } ?>
+                    </div>
+                </form>
+
+
+                <br />
+                <div class="container">
+                    <h2 class="title">Deletar usuário:</h2>
+                    <div class="item">
+                        <label class="form__label"></label>
+                        <select>
+                            <option value="marcel">Marcel</option>
+                            <option value="marcos">Marcos</option>
+                            <option value="rodolfo"></option>
+                            <option value="anderson">Anderson</option>
+                        </select>
+                    </div>
+                    <div class="item">
+                        <button class="botao-cadastrar cadastrar-blue"> <span class="delete" style=" width: 28PX;
+                            height: 28PX;
+                            margin-bottom: -6px;
+                            margin-right: 10px;"></span> Deletar usuário!</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12" id="footer">
+            Olá <span class="span--user">ADM</span>, hoje é dia <span class="span--bold">16/10/2017!</span> O produto mais próximo de vencimento é <span class="span--bold">31/12/2017!</span>
+        </div>
+    </div>
+</div>
+
+
+<!-- Sessão scripts -->
+
+<script type="text/javascript" src="cordova.js"></script>
+<script type="text/javascript" src="scripts/platformOverrides.js"></script>
+<script type="text/javascript" src="scripts/index.js"></script>
+<script type="text/javascript" src="scripts/login.js"></script>
+</body>
+
 </html>
